5068a7cbf9e67fc808eb795aea572353843aed3a
mohamedaouadi726-commits <mohamedaouadi726@gmail.com>
2026-09-23 13:34:11 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,8 +6,8 @@ const PRECACHE_URLS = [
   "./missioncnrt.html",
   "./structurecnrt.html",
   "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", (event) => {
